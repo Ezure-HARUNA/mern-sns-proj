e85e0d3b2771a31d95e74390a7a1a1d173288113
ezu0 <burimofumofu@gmail.com>
2024-10-14 17:12:25 +0900

    feat: 投稿の削除

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -33,4 +33,28 @@ router.put("/:id", async (req, res) => {
   }
 })
 
+//投稿の削除
+router.delete("/:id", async (req, res) => {
+  try {
+  const post = await Post.findById(req.params.id);
+  if (post.userId === req.body.userId) {
+    await post.deleteOne();
+    return res.status(200).json("投稿の削除に成功しました！");
+  } else {
+    return res.status(403).json("あなたはほかの人の投稿を削除できません");
+  }
+  } catch (err) {
+    return res.status(500).json(err)
+  }
+})
+
+//特定の投稿の取得
+router.get("/:id", async (req, res) => {
+  try {
+
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+})
+
 module.exports = router;
