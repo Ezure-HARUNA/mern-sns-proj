12aa23303e74d4a4f9853687cdb464c98a7a99a0
ezu0 <burimofumofu@gmail.com>
2024-10-14 16:54:35 +0900

    feat: 投稿内容の更新

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -16,4 +16,21 @@ router.post("/", async (req, res) => {
   }
 })
 
+//投稿の更新
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if(post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body
+      })
+      return res.status(200).json("投稿の更新に成功しました！")
+    } else {
+      return res.status(403).json("あなたはほかの人の投稿を編集できません")
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+})
+
 module.exports = router;
